5402f126f5fe550b9974e920d3f146911438bcd8
Omolemo_Modise <omolemo.modise.mogudi@gmail.com>
2026-03-18 13:16:39 +0000

    sort word array

--- tryHeapsort.java
+++ tryHeapsort.java
@@ -27,14 +27,39 @@ public static void buildHeapbottomUp(String[] a, int n, int i) {
             buildHeap(words, words.length, 0);
         }
     }
-    // build a heap from top down using insert(node n) 
+    // build a heap from top down using insert 
 
     public static void buildHeaptopDown(String[]a, int n, int i){
-        for int (i = 1; i< a.length; i++){
+        for (int i = 1; i < a.length; i++){
             insert(a,i);
         }
 
-        public static void 
+        public static void insert (String[] a, int i){
+            int parent = (i-1)/2;
+            if(a[i] > a[parent]){
+                String temp = a[i];
+                a[i] = a[parent];
+                a[parent] = temp;
+                insert(a, parent);
+            }
+            // use heap sort to sort the list of words into alphabetical order
+            public static void main(String[] args){
+                String[] words = {"apple", "brush", "cabbage", "pumpkin", "watermelon"};
+                buildHeaptopDown(words, words.length, 0);
+
+                // use timing to compare the two methods of building a heap
+                long startTime = System.nanoTime();
+                buildHeapbottomUp(words, words.length.0);
+                long endTime = System.nanoTime();
+                long duration = endTime - startTime;
+                System.out.println("time taken to build heap from bottom up " + duration + "nanoseconds");
+                startTime = System.nanoTime();
+                buildHeaptopDown(words,words.length,0);
+                endTime = System.nanoTime();
+                duration = endTime - startTime;
+                System.out.println()
+        }
+
 
 
     }
