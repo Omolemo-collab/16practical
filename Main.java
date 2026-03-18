5b1713f16ff07864e5ae4b38ccfec87f2193bcc0
Omolemo_Modise <omolemo.modise.mogudi@gmail.com>
2026-03-18 13:50:36 +0000

    reader added

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
 
@@ -17,11 +21,9 @@ public class Main {
         if (left < n && a[left].compareTo(a[largest]) > 0) {
             largest = left;
         }
-
         if (right < n && a[right].compareTo(a[largest]) > 0) {
             largest = right;
         }
-
         if (largest != i) {
             String temp = a[i];
             a[i] = a[largest];
@@ -31,7 +33,6 @@ public class Main {
         }
     }
 
-    // Top-down heap build
     public static void buildHeapTopDown(String[] a, int n) {
         for (int i = 1; i < n; i++) {
             insert(a, i);
@@ -51,22 +52,30 @@ public class Main {
             insert(a, parent);
         }
     }
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new FileReader("ulysses.txt"));
 
-        String[] words = {"apple", "brush", "cabbage", "pumpkin", "watermelon"};
+        String line;
+        String[] words = new String[1000];
+        int index = 0;
 
+        while ((line = reader.readLine()) != null && index < words.length) {
+            words[index++] = line;
+        }
+        reader.close();
         // Bottom-up timing
         long startTime = System.nanoTime();
-        buildHeapBottomUp(words, words.length);
+        buildHeapBottomUp(words, index);
         long endTime = System.nanoTime();
 
         System.out.println("Bottom-up time: " + (endTime - startTime));
 
+        // Top-down timing
         startTime = System.nanoTime();
-        buildHeapTopDown(words, words.length);
+        buildHeapTopDown(words, index);
         endTime = System.nanoTime();
 
         System.out.println("Top-down time: " + (endTime - startTime));
     }
-}
+}   
